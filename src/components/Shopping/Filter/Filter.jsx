42aa1c3fa90185900eadf8f71e7b07be68a09d87
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2025-05-27 14:51:00 +0300

    some files are deleted

--- src/components/Shopping/Filter/Filter.jsx
+++
@@ -1,9 +0,0 @@
-import css from "./Filter.module.css";
-
-const Filter = ({ filter, onFilterChange }) => {
-  return (
-    <input type="text" value={filter} name="filter" onChange={onFilterChange} />
-  );
-};
-
-export default Filter;
