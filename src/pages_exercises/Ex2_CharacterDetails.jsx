784fc1ad3ce255c32e16dc7930e2e052e85d0b5b
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2025-07-21 17:44:49 +0300

    Ex2_CharacterDetails.jsx and Ex2_CharactersList.jsx in pages_exercises directory are modified

--- src/pages_exercises/Ex2_CharacterDetails.jsx
+++ src/pages_exercises/Ex2_CharacterDetails.jsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 const CharacterDetails = () => {
+  const { id } = useParams();
+  console.log(id);
   return <ul>Character Details</ul>;
 };
 
--- src/pages_exercises/Ex2_CharactersList.jsx
+++ src/pages_exercises/Ex2_CharactersList.jsx
@@ -1,44 +1,55 @@
 import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import styled from "styled-components";
 import { getCharacters } from "../services/sw-characters-api";
 
 const CharactersList = () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [page, setPage] = useState(1);
+  //   const [searchParams, setSearchParams] = useSearchParams();
+  //   const id = searchParams.get("dogId") ?? "";
 
   useEffect(() => {
     const fetchCharacters = async () => {
       try {
         setLoading(true);
         const data = await getCharacters();
-        console.log("items", items);
-        console.log(data);
-        setItems((items) => [...items, ...data]);
+        const dataWithId = data.map((item) => ({
+          ...item,
+          id: item.url.slice(30),
+        }));
+        console.log(dataWithId);
+        setItems([...dataWithId]);
       } catch (error) {
         setError(error);
       } finally {
         setLoading(false);
       }
-      fetchCharacters();
     };
+    fetchCharacters();
   }, []);
 
-  const loadMore = () => {
-    setPage((page) => page + 1);
-  };
+  //   const updateQueryString = (e) => {
+  //     const characterIdValue = e.target.value;
+  //     if (characterIdValue === "") {
+  //       return setSearchParams({});
+  //     }
+  //     setSearchParams({ id: e.target.value });
+  //   };
 
-  const elements = items.map((item) => <Link to="/:id">{item.name}</Link>);
+  const elements = items.map((item) => (
+    <li key={item.id}>
+      <Link to="/:id">{item.name}</Link>
+    </li>
+  ));
 
   return (
     <>
       <h2>Characters List</h2>
-      <ul>{elements}</ul>
+      <ol>{elements}</ol>
       {loading && <p>...loading</p>}
       {error && <p>Failed to load info. Try again later, please.</p>}
-      <button type="button" onClick={loadMore}>
-        Load more
-      </button>
     </>
   );
 };
